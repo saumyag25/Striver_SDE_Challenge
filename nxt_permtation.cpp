171849b7cf0a243e3f5862727ccff01686135f11
saumyag25 <97531898+saumyag25@users.noreply.github.com>
2023-06-09 01:32:36 +0530

    Day -1

---
+++ nxt_permtation.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h> 
+vector<int> nextPermutation(vector<int> &perm, int n)
+{
+    //  Write your code here.
+    next_permutation(perm.begin(),perm.end());
+    return perm;
+}
